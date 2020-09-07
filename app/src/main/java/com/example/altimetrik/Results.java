0115502f7095e8f64556f4949dfcbb19ca1c909a
Rupesh\Ruupu <rkruupesh@gmail.com>
2020-09-08 00:49:11 +0530

    converted json to pojo for retrivivng the data

---
+++ app/src/main/java/com/example/altimetrik/Results.java
@@ -0,0 +1,390 @@
+package com.example.altimetrik;
+
+
+public class Results {
+    private String artworkUrl100;
+
+    private String trackTimeMillis;
+
+    private String longDescription;
+
+    private String trackHdRentalPrice;
+
+    private String country;
+
+    private String previewUrl;
+
+    private String collectionArtistId;
+
+    private String collectionHdPrice;
+
+    private String hasITunesExtras;
+
+    private String trackName;
+
+    private String collectionArtistViewUrl;
+
+    private String collectionName;
+
+    private String discNumber;
+
+    private String trackCount;
+
+    private String artworkUrl30;
+
+    private String wrapperType;
+
+    private String currency;
+
+    private String collectionId;
+
+    private String trackExplicitness;
+
+    private String collectionViewUrl;
+
+    private String trackHdPrice;
+
+    private String contentAdvisoryRating;
+
+    private String trackNumber;
+
+    private String releaseDate;
+
+    private String kind;
+
+    private String trackId;
+
+    private String trackRentalPrice;
+
+    private String collectionPrice;
+
+    private String shortDescription;
+
+    private String discCount;
+
+    private String primaryGenreName;
+
+    private String trackPrice;
+
+    private String collectionExplicitness;
+
+    private String trackViewUrl;
+
+    private String artworkUrl60;
+
+    private String trackCensoredName;
+
+    private String artistName;
+
+    private String collectionCensoredName;
+
+    public String getArtworkUrl100() {
+        return artworkUrl100;
+    }
+
+    public void setArtworkUrl100(String artworkUrl100) {
+        this.artworkUrl100 = artworkUrl100;
+    }
+
+    public String getTrackTimeMillis() {
+        return trackTimeMillis;
+    }
+
+    public void setTrackTimeMillis(String trackTimeMillis) {
+        this.trackTimeMillis = trackTimeMillis;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
+    }
+
+    public String getTrackHdRentalPrice() {
+        return trackHdRentalPrice;
+    }
+
+    public void setTrackHdRentalPrice(String trackHdRentalPrice) {
+        this.trackHdRentalPrice = trackHdRentalPrice;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPreviewUrl() {
+        return previewUrl;
+    }
+
+    public void setPreviewUrl(String previewUrl) {
+        this.previewUrl = previewUrl;
+    }
+
+    public String getCollectionArtistId() {
+        return collectionArtistId;
+    }
+
+    public void setCollectionArtistId(String collectionArtistId) {
+        this.collectionArtistId = collectionArtistId;
+    }
+
+    public String getCollectionHdPrice() {
+        return collectionHdPrice;
+    }
+
+    public void setCollectionHdPrice(String collectionHdPrice) {
+        this.collectionHdPrice = collectionHdPrice;
+    }
+
+    public String getHasITunesExtras() {
+        return hasITunesExtras;
+    }
+
+    public void setHasITunesExtras(String hasITunesExtras) {
+        this.hasITunesExtras = hasITunesExtras;
+    }
+
+    public String getTrackName() {
+        return trackName;
+    }
+
+    public void setTrackName(String trackName) {
+        this.trackName = trackName;
+    }
+
+    public String getCollectionArtistViewUrl() {
+        return collectionArtistViewUrl;
+    }
+
+    public void setCollectionArtistViewUrl(String collectionArtistViewUrl) {
+        this.collectionArtistViewUrl = collectionArtistViewUrl;
+    }
+
+    public String getCollectionName() {
+        return collectionName;
+    }
+
+    public void setCollectionName(String collectionName) {
+        this.collectionName = collectionName;
+    }
+
+    public String getDiscNumber() {
+        return discNumber;
+    }
+
+    public void setDiscNumber(String discNumber) {
+        this.discNumber = discNumber;
+    }
+
+    public String getTrackCount() {
+        return trackCount;
+    }
+
+    public void setTrackCount(String trackCount) {
+        this.trackCount = trackCount;
+    }
+
+    public String getArtworkUrl30() {
+        return artworkUrl30;
+    }
+
+    public void setArtworkUrl30(String artworkUrl30) {
+        this.artworkUrl30 = artworkUrl30;
+    }
+
+    public String getWrapperType() {
+        return wrapperType;
+    }
+
+    public void setWrapperType(String wrapperType) {
+        this.wrapperType = wrapperType;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getCollectionId() {
+        return collectionId;
+    }
+
+    public void setCollectionId(String collectionId) {
+        this.collectionId = collectionId;
+    }
+
+    public String getTrackExplicitness() {
+        return trackExplicitness;
+    }
+
+    public void setTrackExplicitness(String trackExplicitness) {
+        this.trackExplicitness = trackExplicitness;
+    }
+
+    public String getCollectionViewUrl() {
+        return collectionViewUrl;
+    }
+
+    public void setCollectionViewUrl(String collectionViewUrl) {
+        this.collectionViewUrl = collectionViewUrl;
+    }
+
+    public String getTrackHdPrice() {
+        return trackHdPrice;
+    }
+
+    public void setTrackHdPrice(String trackHdPrice) {
+        this.trackHdPrice = trackHdPrice;
+    }
+
+    public String getContentAdvisoryRating() {
+        return contentAdvisoryRating;
+    }
+
+    public void setContentAdvisoryRating(String contentAdvisoryRating) {
+        this.contentAdvisoryRating = contentAdvisoryRating;
+    }
+
+    public String getTrackNumber() {
+        return trackNumber;
+    }
+
+    public void setTrackNumber(String trackNumber) {
+        this.trackNumber = trackNumber;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
+
+    public String getTrackId() {
+        return trackId;
+    }
+
+    public void setTrackId(String trackId) {
+        this.trackId = trackId;
+    }
+
+    public String getTrackRentalPrice() {
+        return trackRentalPrice;
+    }
+
+    public void setTrackRentalPrice(String trackRentalPrice) {
+        this.trackRentalPrice = trackRentalPrice;
+    }
+
+    public String getCollectionPrice() {
+        return collectionPrice;
+    }
+
+    public void setCollectionPrice(String collectionPrice) {
+        this.collectionPrice = collectionPrice;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getDiscCount() {
+        return discCount;
+    }
+
+    public void setDiscCount(String discCount) {
+        this.discCount = discCount;
+    }
+
+    public String getPrimaryGenreName() {
+        return primaryGenreName;
+    }
+
+    public void setPrimaryGenreName(String primaryGenreName) {
+        this.primaryGenreName = primaryGenreName;
+    }
+
+    public String getTrackPrice() {
+        return trackPrice;
+    }
+
+    public void setTrackPrice(String trackPrice) {
+        this.trackPrice = trackPrice;
+    }
+
+    public String getCollectionExplicitness() {
+        return collectionExplicitness;
+    }
+
+    public void setCollectionExplicitness(String collectionExplicitness) {
+        this.collectionExplicitness = collectionExplicitness;
+    }
+
+    public String getTrackViewUrl() {
+        return trackViewUrl;
+    }
+
+    public void setTrackViewUrl(String trackViewUrl) {
+        this.trackViewUrl = trackViewUrl;
+    }
+
+    public String getArtworkUrl60() {
+        return artworkUrl60;
+    }
+
+    public void setArtworkUrl60(String artworkUrl60) {
+        this.artworkUrl60 = artworkUrl60;
+    }
+
+    public String getTrackCensoredName() {
+        return trackCensoredName;
+    }
+
+    public void setTrackCensoredName(String trackCensoredName) {
+        this.trackCensoredName = trackCensoredName;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public String getCollectionCensoredName() {
+        return collectionCensoredName;
+    }
+
+    public void setCollectionCensoredName(String collectionCensoredName) {
+        this.collectionCensoredName = collectionCensoredName;
+    }
+
+    @Override
+    public String toString() {
+        return "ClassPojo [artworkUrl100 = " + artworkUrl100 + ", trackTimeMillis = " + trackTimeMillis + ", longDescription = " + longDescription + ", trackHdRentalPrice = " + trackHdRentalPrice + ", country = " + country + ", previewUrl = " + previewUrl + ", collectionArtistId = " + collectionArtistId + ", collectionHdPrice = " + collectionHdPrice + ", hasITunesExtras = " + hasITunesExtras + ", trackName = " + trackName + ", collectionArtistViewUrl = " + collectionArtistViewUrl + ", collectionName = " + collectionName + ", discNumber = " + discNumber + ", trackCount = " + trackCount + ", artworkUrl30 = " + artworkUrl30 + ", wrapperType = " + wrapperType + ", currency = " + currency + ", collectionId = " + collectionId + ", trackExplicitness = " + trackExplicitness + ", collectionViewUrl = " + collectionViewUrl + ", trackHdPrice = " + trackHdPrice + ", contentAdvisoryRating = " + contentAdvisoryRating + ", trackNumber = " + trackNumber + ", releaseDate = " + releaseDate + ", kind = " + kind + ", trackId = " + trackId + ", trackRentalPrice = " + trackRentalPrice + ", collectionPrice = " + collectionPrice + ", shortDescription = " + shortDescription + ", discCount = " + discCount + ", primaryGenreName = " + primaryGenreName + ", trackPrice = " + trackPrice + ", collectionExplicitness = " + collectionExplicitness + ", trackViewUrl = " + trackViewUrl + ", artworkUrl60 = " + artworkUrl60 + ", trackCensoredName = " + trackCensoredName + ", artistName = " + artistName + ", collectionCensoredName = " + collectionCensoredName + "]";
+    }
+}
+
